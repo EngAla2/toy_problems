99e7198447f8a96559477ea9ee8cce4437f316d5
EngAla2 <Eng4Ala2@gmail.com>
2021-09-22 15:16:24 +0300

    getUniqueValues code

--- unique/index.test.js
+++ unique/index.test.js
@@ -2,7 +2,7 @@
  * Given a single input array, write a function that outputs an unique array of values
  *
  *
- * What time complexity is your solution?
+ * What time complexity is your solution? n
  *
  * Your solution shouldn't using any build in function.
  *
@@ -17,6 +17,18 @@
 const arrOfNum = [1, 2, 2, 4, 5, 6, 6];
 
 const getUniqueValues = (arrOfNum) => {
-  // TO DO
+  let arrOfUniqueNum = []
+  arrOfNum.forEach(ch => {
+    if (! arrOfUniqueNum.includes(ch))
+    arrOfUniqueNum.push(ch)
+  });
+  return arrOfUniqueNum
 }
-console.log(getUniqueValues(arrOfNum)); // [1, 2, 4, 5, 6]
+
+describe("Tests", () => {
+  it("test toSnakeCase", () => {
+    expect(getUniqueValues(arrOfNum)).toStrictEqual([1, 2, 4, 5, 6])
+    expect(getUniqueValues([0,0,0,0,5,2,4,3,2,3,4,5,5])).toStrictEqual([0, 5, 2, 4, 3])
+    expect(getUniqueValues([])).toStrictEqual([])
+  });
+});
