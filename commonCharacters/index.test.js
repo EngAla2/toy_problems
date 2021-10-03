b681319640cd65ca277dd32b4020dc932152de2c
EngAla2 <Eng4Ala2@gmail.com>
2021-10-03 17:35:36 +0300

    resolve commonCharacters

--- commonCharacters/index.test.js
+++ commonCharacters/index.test.js
@@ -11,5 +11,27 @@
  */
 
 const commonCharacters = (str1, str2) => {
-  // TODO
+  let str = new Set()
+  str1 = new Set(str1.replaceAll(" ", "").split(""))
+  str1.forEach(ch => {
+    if (str2.includes(ch))
+    str.add(ch)
+  });
+  return Array.from(str).join("")
 };
+const commonCharactersList = (listOfStr) => {
+  let str0 = listOfStr[0]
+  for (let a = listOfStr.length - 1; a >= 0; a--){
+    str0 = commonCharacters(listOfStr[a], str0)
+  }
+  return str0
+}
+console.log(commonCharactersList(['acexivou', 'aegihobu']))
+
+describe("Tests", () => {
+  it("test commonCharacters", () => {
+    expect(commonCharactersList(['acexivou', 'aegihobu'])).toStrictEqual("aeiou")
+    expect(commonCharactersList(['acexivo  u', 'a  egihobu'])).toStrictEqual("aeiou")
+    expect(commonCharactersList(['acexivou', 'aegihobu', 'awert'])).toStrictEqual("ae")
+  });
+});
