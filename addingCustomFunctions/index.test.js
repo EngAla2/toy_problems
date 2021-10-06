e923985317d14064885023f29784225031493969
EngAla2 <Eng4Ala2@gmail.com>
2021-10-06 10:11:44 +0300

    array custom functions

--- addingCustomFunctions/index.test.js
+++ addingCustomFunctions/index.test.js
@@ -6,4 +6,30 @@
  * 4- search for value
  */
 
-//  TODO
+Array.prototype.sortArray = function(){
+  return this.sort()
+}
+
+Array.prototype.getFirstElement = function(array){
+    return this[0]
+}
+
+Array.prototype.getLastElement = function(array){
+  return this.pop()
+}
+
+
+Array.prototype.searchArray = function(val){
+  return this.find(e => e === val)
+}
+
+
+describe('Tests ', () => {
+  it('test array custom functions', () => {
+    let arr = [1,2,3,0]
+    expect(arr.sortArray()).toEqual([0,1,2,3]);
+    expect(arr.getLastElement()).toEqual(3);
+    expect(arr.getFirstElement()).toEqual(0);
+    expect(arr.searchArray(2)).toEqual(2);
+  });
+});
