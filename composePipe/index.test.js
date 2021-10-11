6f480e1eda19ea4467438189efd60eb2cb0fb919
EngAla2 <Eng4Ala2@gmail.com>
2021-10-11 15:04:44 +0300

    resolve Compose and Pipe

--- composePipe/index.test.js
+++ composePipe/index.test.js
@@ -32,3 +32,34 @@
   var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
   addAndMultiplyTwice(5); //should be 63
  */
+
+const comp = (...args) => {
+  return (arg) => {
+    for (let i=args.length -1; i >= 0; i--) arg = args[i](arg)
+    return arg
+  }
+} 
+
+const pipe = (...args) => {
+  return (arg) => {
+    for (let i=0; i < args.length; i++) arg = args[i](arg)
+    return arg
+  } 
+}
+
+describe('Tests', () => {
+  it('test Compose', () => {
+    var greet = function(name){ return 'hello ' + name;}
+    var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+    var welcome = comp(greet, exclaim);
+    expect(welcome('phillip')).toEqual('hello PHILLIP!');
+  });
+  it('test Pipe', () => {
+    var add2 = function(number){ return number + 2; }
+    var multiplyBy3 = function(number){ return number * 3; }
+    var addAndMultiply = pipe(add2, multiplyBy3);
+    expect(addAndMultiply(5)).toEqual(21);
+    var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
+    expect(addAndMultiplyTwice(5)).toEqual(63);
+  });
+});
