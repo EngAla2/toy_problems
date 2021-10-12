10c59ffc3d6bd4f902503f98c81554e3ff4117cf
EngAla2 <Eng4Ala2@gmail.com>
2021-10-12 12:00:53 +0300

    add - tests

--- rotate/index.test.js
+++ rotate/index.test.js
@@ -40,6 +40,10 @@ describe('Tests', () => {
     expect(rotate(['a', 'b', 'c'], 3)).toEqual(['a', 'b', 'c']);
     expect(rotate([1, 2, 3, 4, 5], 7)).toEqual([4, 5, 1, 2, 3]);
     expect(rotate([1, 2, 3, 4, 5], 11)).toEqual([5, 1, 2, 3, 4]);
-    expect(rotate([1, 2, 3, 4, 5], 12478)).toEqual([3, 4, 5, 1, 2]);    
+    expect(rotate([1, 2, 3, 4, 5], 12478)).toEqual([3, 4, 5, 1, 2]); 
+    expect(rotate([1, 2, 3, 4, 5], 0)).toEqual([1, 2, 3, 4, 5]);
+    expect(rotate([1, 2, 3, 4, 5], -1)).toEqual([2, 3, 4, 5, 1]);
+    expect(rotate([1, 2, 3, 4, 5], -2)).toEqual([3, 4, 5, 1, 2]);    
+    expect(rotate([1, 2, 3, 4, 5], -5)).toEqual([1, 2, 3, 4, 5]);    
   });
 });
